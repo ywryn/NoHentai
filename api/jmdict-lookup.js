bce15849e87c5bc6c74ca393471aafb3e1b43342
YarrowRen <ywrby0214@gmail.com>
2026-05-12 11:17:35 +0800

    fix(jmdict-lookup): use wildcard CORS instead of origin whitelist

--- api/jmdict-lookup.js
+++ api/jmdict-lookup.js
@@ -1,9 +1,8 @@
-import { setCors, handleOptions } from './_cors.js'
-
 export default async function handler(req, res) {
-  if (handleOptions(req, res)) return
-  setCors(req, res)
-
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  if (req.method === 'OPTIONS') return res.status(204).end()
   if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' })
 
   const { word } = req.query
